440b71ac7c30a3b16bbc07150568076e5357d8d9
Quantin <83812796+lmntrix022@users.noreply.github.com>
2025-04-28 02:26:58 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://llnjcsanbtxrjisontsd.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxsbmpjc2FuYnR4cmppc29udHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTI0MzEsImV4cCI6MjA2MTA4ODQzMX0.lo13jAtQwGcMHbdijCXM-qujz-pHpYVOR_90V4HQdFI";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
